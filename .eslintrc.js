55fe5821fafb29741ae4684bef4aa40555fa71e3
Sieu Tsoi <sieut@umich.edu>
2021-12-12 11:22:01 -0800

    Update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,18 +30,24 @@ module.exports = {
     },
     rules: {
         "@typescript-eslint/array-type": "error",
+        "@typescript-eslint/ban-ts-comment": "off",
         "@typescript-eslint/consistent-type-assertions": "error",
         "@typescript-eslint/consistent-type-definitions": "error",
         "@typescript-eslint/explicit-function-return-type": "off",
         "@typescript-eslint/explicit-member-accessibility": [
             "error",
             {
-                accessibility: "explicit"
+                accessibility: "explicit",
+                overrides: { constructors: "no-public" },
             }
         ],
+        "@typescript-eslint/no-empty-function": "off",
+        "@typescript-eslint/no-empty-interface": "off",
         "@typescript-eslint/no-explicit-any": "off",
         "@typescript-eslint/no-namespace": "off",
+        "@typescript-eslint/no-non-null-assertion": "off",
         "@typescript-eslint/no-parameter-properties": "off",
+        "@typescript-eslint/no-unnecessary-type-assertion": "off",
         "@typescript-eslint/no-unused-expressions": "error",
         "@typescript-eslint/no-use-before-define": ["error", { functions: false }],
         "@typescript-eslint/prefer-for-of": "error",
@@ -57,7 +63,6 @@ module.exports = {
         "id-blacklist": ["error", "any", "Number", "number", "String", "string", "Boolean", "boolean", "Undefined"],
         "id-match": "error",
         "linebreak-style": "off",
-        "max-classes-per-file": ["error", 1],
         "new-parens": "off",
         "newline-per-chained-call": "off",
         "no-bitwise": "error",
