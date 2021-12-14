2896c0ff3a4bef514f904da9630f959c257a1bcf
Sieu Tsoi <sieut@umich.edu>
2021-12-13 18:41:57 -0800

    Take sudo out from docker-compose commands

--- screeps.sh
+++ screeps.sh
@@ -2,21 +2,21 @@
 
 case $1 in
     up)
-        sudo docker-compose up -d
+        docker-compose up -d
         ;;
     cli)
-        sudo docker-compose exec screeps screeps-launcher cli
+        docker-compose exec screeps screeps-launcher cli
         ;;
     lint)
-        sudo docker-compose exec $2 dev npm run lint
+        docker-compose exec $2 dev npm run lint
         ;;
     prettier)
-        sudo docker-compose exec $2 dev npm run prettier
+        docker-compose exec $2 dev npm run prettier
         ;;
     test)
-        sudo docker-compose exec $2 dev npm run test
+        docker-compose exec $2 dev npm run test
         ;;
     *)
-        sudo docker-compose "$@"
+        docker-compose "$@"
         ;;
 esac
